ef6843fac373a1ea8fc43e57deb6f28c451afc43
MariaBarinova <96283911+MariaBarinova@users.noreply.github.com>
2023-01-12 19:55:54 +0300

    Add files via upload

---
+++ Migrations/20221224172034_Company.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lab3.Migrations
+{
+    /// <inheritdoc />
+    public partial class Company : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Catalogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    WorkTitle = table.Column<string>(name: "Work_Title", type: "nvarchar(max)", nullable: false),
+                    Cost = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Catalogs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Identitynumber = table.Column<int>(name: "Identity_number", type: "int", nullable: false),
+                    Department = table.Column<int>(type: "int", nullable: false),
+                    Post = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Jobid = table.Column<int>(name: "Job_id", type: "int", nullable: false),
+                    catalogId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Employees_Catalogs_catalogId",
+                        column: x => x.catalogId,
+                        principalTable: "Catalogs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_catalogId",
+                table: "Employees",
+                column: "catalogId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "Catalogs");
+        }
+    }
+}
